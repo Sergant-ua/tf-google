ba584205105e0ffcaab509929d9ef6593712af40
Serhi Povazhniuk <sergiy.povazhnyuk@gmail.com>
2024-09-08 14:03:22 +0000

    Updated code for create GKE with Flux

--- variables.tf
+++ variables.tf
@@ -12,11 +12,10 @@ variable "GOOGLE_REGION" {
 
 variable "GKE_NUM_NODES" {
   type        = number
-  default     = 1
+  default     = 4
   description = "GKE nodes number"
 
 }
-
 
 variable "GKE_MACHINE_TYPE" {
   type        = string
@@ -34,4 +33,26 @@ variable "GKE_POOL_NAME" {
   type        = string
   default     = "main"
   description = "GKE pool name"
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitHub repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
 }
