442a774b8aa7e302e8065a09fd4e217614c295af
Serhi Povazhniuk <sergiy.povazhnyuk@gmail.com>
2024-09-09 21:50:29 +0000

    Updated terraform modules

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket  = "sergant-bucket"
+    prefix  = "terraform/state"
+  }
+}
+
 module "github_repository" {
   source                   = "github.com/den-vasyliev/tf-github-repository"
   github_owner             = var.GITHUB_OWNER
@@ -8,18 +15,21 @@ module "github_repository" {
 }
 
 module "gke_cluster" {
-  source           = "github.com/den-vasyliev/tf-google-gke-cluster"
+  source           = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
   GOOGLE_REGION    = var.GOOGLE_REGION
   GOOGLE_PROJECT   = var.GOOGLE_PROJECT
   GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
-  GKE_NUM_NODES    = 1
+  GKE_NUM_NODES    = 2
 }
 
 module "flux_bootstrap" {
-  source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.GITHUB_REPO}"
-  private_key = module.tls_private_key.private_key_pem
-  config_path = module.gke_cluster.kubeconfig
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_host       = module.gke_cluster.config_host
+  config_token      = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
+  github_token      = var.GITHUB_TOKEN
 }
 
 module "tls_private_key" {
